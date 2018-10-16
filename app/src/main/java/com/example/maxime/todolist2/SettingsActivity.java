ef377852607e84cc9dc1511983a49715896d2b8b
maximeNicot <maximenicot@outlook.com>
2018-10-16 16:42:55 +0200

    d'avantage de settings

--- app/src/main/java/com/example/maxime/todolist2/SettingsActivity.java
+++ app/src/main/java/com/example/maxime/todolist2/SettingsActivity.java
@@ -15,5 +15,21 @@ public class SettingsActivity extends PreferenceActivity {
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
     }
+/*<PreferenceCategory
+            android:key="pref_key_general_settings"
+            android:title="@string/pref_sync_google_facebook_title">
+            <EditTextPreference
+                android:dependency="pref_sync"
+                android:defaultValue="true"
+                android:key="pref_sync"
+                android:summary="@string/pref_sync_sum_google"
+                android:title="@string/pref_sync_google" />
+            <EditTextPreference
+                android:dependency="pref_sync"
+                android:defaultValue="true"
+                android:key="pref_sync"
+                android:summary="@string/pref_sync_sum_facebook"
+                android:title="@string/pref_sync_facebook" />
 
+        </PreferenceCategory>*/
 }
